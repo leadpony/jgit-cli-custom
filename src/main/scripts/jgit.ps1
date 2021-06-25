db77c345d6acaa70338d568fa5792fd14e24a1f8
leadpony <hacker@leadpony.org>
2021-06-25 20:22:26 +0900

    Add paging for blame, diff, log

--- src/main/scripts/jgit.ps1
+++ src/main/scripts/jgit.ps1
@@ -1,12 +1,26 @@
 ﻿$scriptPath = $MyInvocation.MyCommand.Path
 $baseDir = Split-Path $scriptPath -Parent
 
+$cmd = $null
+foreach ($arg in $args) {
+    if (-not $arg.StartsWith('-')) {
+        $cmd = $arg
+        break
+    }    
+}
+
+$usePager = @('blame', 'diff', 'log').Contains($cmd)
+
 $java = "java"
 if ($null -ne $env:JAVA_HOME) {
     $java = "$env:JAVA_HOME\bin\java"
 }
 
-$javaArgs = @('-jar', "$baseDir\jgit-cli-${jgit.version}.jar") + $Args
+$javaArgs = @('-jar', "$baseDir\jgit-cli-${jgit.version}.jar") + $args
 $javaArgs = $javaArgs | ForEach-Object { """$_""" } 
 
-Start-Process -FilePath $java -ArgumentList $javaArgs -Wait -NoNewWindow
+if ($usePager) {
+    & $java $javaArgs | Out-Host -Paging
+} else {
+    & $java $javaArgs
+}
