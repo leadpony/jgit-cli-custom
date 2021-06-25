871220c9983f3a06570ccd822e6044d94d2f8b84
leadpony <hacker@leadpony.org>
2021-06-25 19:21:44 +0900

    Add launcher script

---
+++ src/main/scripts/jgit.ps1
@@ -0,0 +1,12 @@
+﻿$scriptPath = $MyInvocation.MyCommand.Path
+$baseDir = Split-Path $scriptPath -Parent
+
+$java = "java"
+if ($null -ne $env:JAVA_HOME) {
+    $java = "$env:JAVA_HOME\bin\java"
+}
+
+$javaArgs = @('-jar', "$baseDir\jgit-cli-${jgit.version}.jar") + $Args
+$javaArgs = $javaArgs | ForEach-Object { """$_""" } 
+
+Start-Process -FilePath $java -ArgumentList $javaArgs -Wait -NoNewWindow
